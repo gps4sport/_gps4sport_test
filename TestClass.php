f6ac7d79306ea7b2d90a95033d29de6b8a9ea632
tybul <gps4sport@gmail.com>
2013-04-28 20:19:48 +0200

    commit z idea

---
+++ TestClass.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tybul
+ * Date: 28.04.13
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */ 
+class TestClass {
+
+}
